f2da9134180d57d0b92838af22715ebbb5369cde
Devin Zacharias <devinzacharias@gmail.com>
2023-05-13 16:47:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import random
 import os
 from art import logo
 
+
 suits = ['♥', '♦', '♣', '♠']
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
+
 
 class Player:
     def __init__(self, name):
@@ -40,6 +42,7 @@ class Player:
         for line in card_lines:
             print(' '.join(line))
         print(f"Total: {self.points}\n")
+
 
 while True:
     print(logo)
